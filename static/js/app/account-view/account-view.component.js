f93cab99dbdf8ac26ac35cd0b851fc13cbf28873
James-L <James-L>
2017-08-26 14:29:55 +0100

    Added weekly routine table

--- static/js/app/account-view/account-view.component.js
+++ static/js/app/account-view/account-view.component.js
@@ -1,56 +1,118 @@
 'use strict';
 
-angular.module('accountView').component('accountView',{
+angular.module('accountView').component('accountView', {
     templateUrl: 'api/templates/accountview.html',
-    controller: function($cookies, $scope, $location, $http, $routeParams){
+    controller: function($cookies, $scope, $location, $http, $routeParams, RoutineData) {
 
-            $scope.updatePassword = function(){
-                var username = $cookies.get('username');
-                $http({
-                    method: 'PUT',
-                    url: '/api/users/' + username + '/' + 'changepassword' + '/',
-                    data: {
-                        "old_password": $scope.passwordChange.oldPassword,
-                        "new_password": $scope.passwordChange.newPassword
-                    },
-                    headers: {'Authorization': 'JWT ' + $cookies.get('token')}
-                }).then(function(response){
-                    $scope.success = true;
-                }, function(response){
-                    $scope.errors = "";
-                    console.log(response)
-                    var data = response.data
-                    $scope.passwordChangeErrors = []
-                    for(var i=0; i<Object.keys(data).length; i++){
-                        var key = Object.keys(data)[i]
-                        $scope.passwordChangeErrors.push(key + ": " + data[key][0])
+        var genRoutineSchedule = function() {
+            var routine = RoutineData.query({
+                "id": $scope.routineID
+            }, function(success){
+                var increment = Math.round(7 / routine.days_per_week);
+                var daysOfWeek = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+                var exampleWeek = {"Mon": [" ",["Rest"]], "Tue": [" ",["Rest"]], "Wed": [" ",["Rest"]],
+                                   "Thu": [" ",["Rest"]], "Fri": [" ",["Rest"]], "Sat": [" ",["Rest"]],
+                                   "Sun": [" ",["Rest"]]};
+                var pointer = 0
+                var counter = 0
+                for(var i=0; i<7; i+=increment){
+                    if(routine.days_per_week == counter){
+                        break
+                    }
+                    var day = daysOfWeek[i];
+                    var exercises = [];
+                    for(var j=0; j<routine.workout[pointer].exercises.length; j++){
+                        exercises.push(routine.workout[pointer].exercises[j].name)
+                    }
+                    exampleWeek[day] = [routine.workout[pointer].name, exercises];
+                    if(pointer+1 >= routine.workout.length){
+                       pointer = 0
+                    } else {
+                       pointer++
+                    }
+                    counter++
                 }
-                })
-            }
-
-            var loggedIn = $cookies.get("token")
-            if(loggedIn == null){
-                $scope.errors = "Please login to view this page"
-            }
-            else {
-                $scope.username = $cookies.get("username")
-                $http({
-                    method: 'GET',
-                    url: '/api/users/' + $scope.username + '/',
-                    headers: {"Authorization": "JWT " + $cookies.get("token")}
-                }).then(function(response){
-                    $scope.data = response.data.user
-                    $scope.firstName = response.data.user.first_name
-                    $scope.routineName = response.data.routine.name
-                    $scope.email = response.data.user.email
-                }, function(response){
-                    console.log(response)
-                })
-                $scope.passwordChange = {
-                    "oldPassword": "",
-                    "newPassword": "",
-                }
-
-            }
+                $scope.daysOfWeek = daysOfWeek;
+                $scope.exampleWeek = exampleWeek;
+                console.log(exampleWeek)
+//                var pointer = 0
+//                var index = 0
+//                while (Object.keys(exampleWeek).length != routine.days_per_week) {
+//                    var day = daysOfWeek[pointer];
+//                    exampleWeek[day] = [routine.workout[index].name, routine.workout[index].exercises];
+//                    pointer += increment
+//                    if(index+1 >= routine.workout.length){
+//                        index = 0
+//                    } else {
+//                        index++
+//                    }
+//                }
+//                console.log(exampleWeek)
+            });
+//            console.log(routine.days_per_week)
+//            var increment = Math.round(7 / routine.days_per_week);
+//            var daysOfWeek = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+//            var exampleWeek = {};
+//            var pointer = 0
+//            while (Object.keys(exampleWeek).length != routine.days_per_week) {
+//                var day = daysOfWeek[pointer];
+//                exampleWeek[day] = routine.workout[Object.keys(exampleWeek).length].name
+//                pointer++
+//            }
+//            console.log(exampleWeek)
         }
-    })
+
+        $scope.updatePassword = function() {
+            var username = $cookies.get('username');
+            $http({
+                method: 'PUT',
+                url: '/api/users/' + username + '/' + 'changepassword' + '/',
+                data: {
+                    "old_password": $scope.passwordChange.oldPassword,
+                    "new_password": $scope.passwordChange.newPassword
+                },
+                headers: {
+                    'Authorization': 'JWT ' + $cookies.get('token')
+                }
+            }).then(function(response) {
+                $scope.success = true;
+            }, function(response) {
+                $scope.errors = "";
+                var data = response.data
+                $scope.passwordChangeErrors = []
+                for (var i = 0; i < Object.keys(data).length; i++) {
+                    var key = Object.keys(data)[i]
+                    $scope.passwordChangeErrors.push(key + ": " + data[key][0])
+                }
+            })
+        }
+
+        var loggedIn = $cookies.get("token")
+        if (loggedIn == null) {
+            $scope.errors = "Please login to view this page"
+        } else {
+            $scope.username = $cookies.get("username")
+            $http({
+                method: 'GET',
+                url: '/api/users/' + $scope.username + '/',
+                headers: {
+                    "Authorization": "JWT " + $cookies.get("token")
+                }
+            }).then(function(response) {
+                $scope.data = response.data.user
+                $scope.firstName = response.data.user.first_name
+                $scope.routineName = response.data.routine.name
+                $scope.email = response.data.user.email
+                $scope.routineID = response.data.routine.id
+                genRoutineSchedule()
+            }, function(response) {
+                console.log(response)
+            })
+            $scope.passwordChange = {
+                "oldPassword": "",
+                "newPassword": "",
+            }
+
+        }
+    }
+})
--- static/js/app/progress/progress.component.js
+++ static/js/app/progress/progress.component.js
@@ -16,7 +16,7 @@ angular.module('progressView').component('progressView', {
                 var token = $cookies.get("token")
 
                 $scope.data = [[]] // Data the graph will be generated from
-                $scope.labels = [] // x axis label ticks for graph
+                //$scope.labels = [] // x axis label ticks for graph
 
                 $scope.exercises = []
                 $scope.allExercises = []
@@ -47,14 +47,15 @@ angular.module('progressView').component('progressView', {
                             $scope.exercises.push([data[i].exercise_id, data[i].exercise_name])
                             }
                         }
+                        //$scope.selectedExercise = $scope.exercises[0]
                     })
 
                 $scope.selectedExercise = [1, "benchpress 5x5"];
 
-                $scope.progressList = []
-
-
                 var genGraph = function(){
+                    // Setup scope variables for angularcharts
+                    // labels will currently hold integers representing individual entried. Ideally holds dates
+                    // however dates take up too much screen realestate on vertical mobile devices.
                     // Reset graph data
                     $scope.data = [[]]
                     $scope.labels = []
@@ -66,12 +67,13 @@ angular.module('progressView').component('progressView', {
                                 if(a.date > b.date) return 1;
                                 return 0;
                         })
+                        $scope.progressList = []
                         for(var i=0; i < data.length; i++){
                             $scope.progressList.push([data[i].id,
                                                    data[i].date + " " + data[i].exercise_name + " " + data[i].quantity])
                             if(data[i].exercise_id == $scope.selectedExercise[0]){
                                 $scope.data[0].push(data[i].quantity)
-                                $scope.labels.push(data[i].date)
+                                $scope.labels.push(i)//data[i].date)
                             }
                         }
                     })
@@ -80,6 +82,7 @@ angular.module('progressView').component('progressView', {
                 $scope.$watch('selectedExercise', genGraph)
 
                 $scope.postProgress = function(){
+                    // Post progress and generate appropriate user feedback messages
                     $scope.postSuccMsg = ""
                     $scope.postErrMsg = ""
                     $http.post('/api/progress/', {
@@ -95,6 +98,7 @@ angular.module('progressView').component('progressView', {
                 }
 
                $scope.deleteProgress = function(){
+                    // Delete given progress and generate appropriate user feedback messages
                     if($scope.selectedProgress){
                         for(var i=0; i<$scope.selectedProgress.length; i++){
                             $http({
